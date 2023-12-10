e5b5c71584d0bb79504479a2ce4a8af2f4b7d51c
Rajesh Joshi <joshirajesh448@gmail.com>
2023-12-10 15:43:32 +0530

    TAS-12 | Added basic retry functionality to the task

---
+++ api/config.py
@@ -0,0 +1,6 @@
+from os import environ as env
+
+JITTER_MAX_RETRIES = int(env.get("JITTER_MAX_RETRIES", "10"))
+JITTER_RETRY_BACKOFF = bool(int(env.get("JITTER_RETRY_BACKOFF", "1")))
+JITTER_RETRY_BACKOFF_MAX = int(env.get("JITTER_RETRY_BACKOFF_MAX", "500"))
+JITTER_RETRY_JITTER = bool(int(env.get("JITTER_RETRY_JITTER", "0")))
