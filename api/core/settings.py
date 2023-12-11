22d8f64e9c3243a06322da2b4be2ce3e517a43d4
Rajesh Joshi <joshirajesh448@gmail.com>
2023-12-11 20:52:10 +0530

    TAS-14 | Made Production Ready

--- api/core/settings.py
+++ api/core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(env.get("DEBUG", "0")))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
